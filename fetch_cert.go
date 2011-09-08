81c159b7c6239e2372e449a0bcd01cce5214deea
Jesse McNelis <jessta@jessta.id.au>
2011-09-08 20:00:15 +1000

    cleaning up

--- fetch_cert.go
+++ fetch_cert.go
@@ -1,113 +1,70 @@
 package main
 
-/*
-	recieve cert from connecting client
-	fetch cert or fetch from db and check whether to renew
-	compare sha1s
-*/
-
-
 import (
 	"crypto/tls"
-	"net"
-	"fmt"
-	"crypto"
-	"os"
 	"crypto/x509"
 	"crypto/rsa"
 	"crypto/rand"
+
 	"encoding/hex"
 	"encoding/base64"
 	"encoding/pem"
+
+	"io/ioutil"
+
 	"http"
 	"path"
 	"strings"
 	"json"
 	"bytes"
-	"io/ioutil"
 	"log"
 	"time"
+	"sync"
+	"net"
+	"fmt"
+	"crypto"
+	"os"
+	"flag"
 )
 
 var PrivateKey *rsa.PrivateKey
 
-/*func init(){
-	/*dec := base64.NewDecoder(base64.StdEncoding,os.Stdin)
-        certBytes,err := ioutil.ReadAll(dec)
-        if err != nil {
-                return nil, err
-        }
-	certs, err := x509.ParseCertificate(certBytes)
-        if err != nil {
-                return nil, err
-        }
-	PrivateKey = certs[0].PrivateKey
-
-
-	PrivateKey =  rsa.GenerateKey(rand.Read, 1024)
-}*/
-
-var config = &tls.Config{
-	// Rand provides the source of entropy for nonces and RSA blinding.
-	// If Rand is nil, TLS uses the cryptographic random reader in package
-	// crypto/rand.
-	//Rand io.Reader
-	nil,
-	// Time returns the current time as the number of seconds since the epoch.
-	// If Time is nil, TLS uses the system time.Seconds.
-	//Time func() int64
-	nil,
-	// Certificates contains one or more certificate chains
-	// to present to the other side of the connection.
-	// Server configurations must include at least one certificate.
-	//Certificates []Certificate
-	nil,
-	// RootCAs defines the set of root certificate authorities
-	// that clients use when verifying server certificates.
-	// If RootCAs is nil, TLS uses the host's root CA set.
-	//RootCAs *x509.CertPool
-	nil,
-	// NextProtos is a list of supported, application level protocols.
-	//NextProtos []string
-	[]string{"http"},
-	// ServerName is included in the client's handshake to support virtual
-	// hosting.
-	"github.com",
-
-	// AuthenticateClient controls whether a server will request a certificate
-	// from the client. It does not require that the client send a
-	// certificate nor does it require that the certificate sent be
-	// anything more than self-signed.
-	false,
-
-	// CipherSuites is a list of supported cipher suites. If CipherSuites
-	// is nil, TLS uses a list of suites supported by the implementation.
-	nil,
-}
 
 type Cert struct {
 	cert *x509.Certificate
 	sum  []byte
 }
 
-var certs = map[string]*Cert{}
+type SyncCertMap struct {
+	certs map[string]*Cert
+	*sync.Mutex
+}
+
+var certs = SyncCertMap{map[string]*Cert{},new(sync.Mutex)}
 
 func FetchCert(address string) (*Cert, os.Error) {
 	//is cert in db?
-	cert, ok := certs[address]
+	certs.Lock()
+	cert, ok := certs.certs[address]
+	certs.Unlock()
+
 	if ok {
 		return cert, nil
 	}
 
 	//fetch cert over network
-	addr, err := net.ResolveTCPAddr("tcp4", address)
+	addr, err := net.ResolveTCPAddr("tcp", address)
 	if err != nil {
 		return nil, err
 	}
-	netCon, err := net.DialTCP("tcp4", nil, addr)
+	netCon, err := net.DialTCP("tcp", nil, addr)
 	if err != nil {
 		return nil, err
 	}
+	host := strings.Split(address,":")[0]
+
+	config := &tls.Config{nil,nil,nil,nil,[]string{"http"},host,false,nil}
+
 	client := tls.Client(netCon, config)
 	err = client.Handshake()
 	if err != nil {
@@ -124,8 +81,18 @@ func FetchCert(address string) (*Cert, os.Error) {
 
 	client.Close()
 	netCon.Close()
-	certs[address] = &Cert{state.PeerCertificates[0], sha.Sum()}
-	return certs[address], nil
+
+	certs.Lock()
+	cert, ok = certs.certs[address]
+	if ok {
+		certs.Unlock()
+		return cert, nil
+	}
+	c := &Cert{state.PeerCertificates[0], sha.Sum()}
+	certs.certs[address] = c
+
+	certs.Unlock()
+	return c, nil
 }
 
 type Response struct {
@@ -144,23 +111,23 @@ type timestamp struct {
 }
 
 func (r Response) MarshalJSON() ([]byte, os.Error) {
-	log.Println("marshalling response")
+	
 	buf := new(bytes.Buffer)
 	buf.Write([]byte(`{"fingerprintList":`))
 	fpList, err := json.Marshal(r.fingerprintList)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	buf.Write(fpList)
 	buf.Write([]byte(`,"signature":"`))
 	signature, err := signResponse(fpList)
 	if err != nil {
-		panic(err)
+		return nil,err
 	}
 	buf.Write([]byte(base64.StdEncoding.EncodeToString(signature)))
 	buf.Write([]byte(`"}`))
-	log.Println(string(buf.Bytes()))
+
 	return buf.Bytes(), nil
 }
 
@@ -172,26 +139,42 @@ func signResponse(response []byte) ([]byte, os.Error) {
 	return rsa.SignPKCS1v15(rand.Reader, PrivateKey, crypto.SHA1, hashed)
 }
 
+
+
 func main() {
 	var err os.Error
-	certBytes, err := ioutil.ReadFile("./key.pem")
-	p, _ := pem.Decode(certBytes)
-	certBytes = p.Bytes
+
+	var keyFile  string
+	var certFile string
+	var addr string
+	flag.StringVar(&keyFile, "keyfile", "./key.pem","file name of key file")
+	flag.StringVar(&certFile, "certfile", "./cert.pem","file name of cert file")
+	flag.StringVar(&addr, "addr", "0.0.0.0:443","address to listen on")
+
+	flag.Parse()
+
+	certBytes, err := ioutil.ReadFile(keyFile)
 	if err != nil {
-		panic(err)
+		log.Fatalln("error reading keyfile: ",err)
 	}
+	p, _ := pem.Decode(certBytes)
+	if p == nil {
+		log.Fatalln("invalid key file")
+	}
+	certBytes = p.Bytes
+	
 	PrivateKey, err = x509.ParsePKCS1PrivateKey(certBytes)
 
 	if err != nil {
-		panic(err)
+		log.Fatalln("invalid key file: ", err)
 	}
+
 	http.HandleFunc("/target/", handleTargetRequest)
-	certFile := "./cert.pem"
-	keyFile := "./key.pem"
+
 	log.Println("listening.....")
-	err = http.ListenAndServeTLS("127.0.0.1:443", certFile, keyFile, nil)
+	err = http.ListenAndServeTLS(addr, certFile, keyFile, nil)
 	if err != nil {
-		panic(err)
+		log.Fatalln("error listening: ",err)
 	}
 }
 
@@ -202,48 +185,51 @@ func handleTargetRequest(w http.ResponseWriter, r *http.Request) {
 	url := path.Clean(r.URL.Path)
 	target := url[strings.LastIndex(url, "/")+1:]
 	a := strings.Split(target, "+")
+	if len(a) != 2 {
+		if len(a) == 0 {
+			http.Error(w, "needs host", http.StatusBadRequest)
+		}
+		if len(a) == 1 {
+			http.Error(w, "needs port", http.StatusBadRequest)
+		}
+		if len(a) > 2 {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+		}
+		return
+	}
 	host, port := a[0], a[1]
 	err := r.ParseForm()
+
 	if err != nil {
-		w.Write([]byte("problem parsing"))
+		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
-	log.Println(r.Form)
+
 	fingerprints, ok := r.Form["fingerprint"]
+
 	if !ok {
 		//missing finger print
-		w.Write([]byte("needs a fingerprint"))
+		http.Error(w, "needs a fingerprint", http.StatusBadRequest)
 		return
 	}
+
 	theirFP := fingerprints[0]
-	log.Println("fetching cert")
+	
 	c, err := FetchCert(host + ":" + port)
 	if err != nil {
-		panic(err)
+		log.Println(err)
+		http.Error(w, "Internal Server Error",http.StatusInternalServerError)
+		return
 	}
-	myFP := hex.EncodeToString(c.sum)
-	myFP = strings.ToUpper(myFP)
-	buf := make([]int, 0, len(myFP))
-	colon := false
-	for i, r := range myFP {
-		buf = append(buf, r)
-		if colon {
-			buf = append(buf, ':')
-			colon = false
-		} else {
 
-			if i < len(myFP)-2 {
-				colon = true
-			}
-		}
-	}
-	myFP = string(buf)
+	myFP := hex.EncodeToString(c.sum)
+	myFP = FingerPrintStr(myFP).String()
 
 	if myFP != theirFP {
 		// maybe I'm out of date?
-		//fetch new and try again.
-		println("They don't match!!!!!!!!!!!!!!!!!!!!!!!!!!")
+		w.WriteHeader(http.StatusConflict)
 	}
+
 	log.Println(host, ":", port, ":", theirFP)
 	c, err = FetchCert(host + ":" + port)
 	d := Response{
@@ -255,11 +241,34 @@ func handleTargetRequest(w http.ResponseWriter, r *http.Request) {
 				Fingerprint: myFP}}}
 	response, err := json.Marshal(d)
 	if err != nil {
-		panic(err)
+		log.Println(err)
+		http.Error(w, "Internal Server Error",http.StatusInternalServerError)
 	}
-	log.Println(response)
 	w.Write(response)
 
 	fmt.Println(hex.EncodeToString(c.sum))
 
 }
+
+type FingerPrintStr string
+
+func (f FingerPrintStr) String() string {
+	str := strings.ToUpper(string(f))
+	buf := make([]int, 0, len(str))
+	colon := false
+	for i, r := range str {
+		buf = append(buf, r)
+		if colon {
+			buf = append(buf, ':')
+			colon = false
+		} else {
+
+			if i < len(f)-2 {
+				colon = true
+			}
+		}
+	}
+	return string(buf)
+}
+
+
